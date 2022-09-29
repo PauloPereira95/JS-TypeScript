91380ba68ebc6ed9c47c529cc01fc899ebfb3afe
PauloPereira95 <paulohuawei95@gmail.com>
2022-09-29 21:26:06 +0100

    Create index.js

---
+++ JavaScript/Aula 21 Operadores de Comparação/index.js
@@ -0,0 +1,15 @@
+/*
+Operadores de comparação
+>   maior que
+>=  maior ou igual
+<   menor
+<=  menor que 
+==  igualdade (valor)
+=== igualdade restrita (valor e tipo) // recomendado
+!=  diferente (valor)   
+!== diferente restrito (valor e tipo) // recomendado
+*/
+let num1 = 10; //Number
+let num2 = '10'; // string
+let comp = num1 !== num2;
+console.log(comp);
