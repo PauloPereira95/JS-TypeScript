5a8f89a83234bdf7519ba617ce0f806db0c72b5f
PauloPereira95 <paulohuawei95@gmail.com>
2022-12-07 20:52:52 +0000

    aula64

---
+++ JavaScript/Aula 64 Concatenar arrays/script.js
@@ -0,0 +1,10 @@
+const a1 = [1,2,3];
+const a2 = [4,5,6];
+//const a3 = a1.concat(a2); // junta os 2 ou mais arrays arrays (a1+a2);
+//const a3 = a1.concat(a2,[7,8,9,'luiz']);
+//------------
+// operador rest ... (spread)
+const a3 = [...a1,...a2];
+
+
+console.log(a3);
