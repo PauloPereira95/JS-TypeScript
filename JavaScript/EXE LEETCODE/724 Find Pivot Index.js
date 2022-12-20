264dca5a124678e17ac4e321f9c33e0037f25424
PauloPereira95 <paulohuawei95@gmail.com>
2022-12-20 00:42:45 +0000

    solucao leet cod pivot

---
+++ JavaScript/EXE LEETCODE/724 Find Pivot Index.js
@@ -0,0 +1,14 @@
+
+var pivotIndex = function(nums) {
+   let totalSum = 0;
+   leftSum = 0;
+   nums.forEach((e) => {
+    totalSum +=e;
+   });
+   for (let i = 0; i < nums.length; i++) {
+    if(totalSum - leftSum - nums[i] === leftSum) return i;
+    leftSum += nums[i];
+   }
+   return -1;
+   
+};
