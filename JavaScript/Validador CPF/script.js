2f5f50aac1465910ce3944d408306cd669708d09
PauloPereira95 <paulohuawei95@gmail.com>
2023-01-19 21:36:30 +0000

    correcao

--- JavaScript/Validador CPF/script.js
+++ JavaScript/Validador CPF/script.js
@@ -1,42 +1,65 @@
-function ValidaCPF(cpfEnviado){
-    Object.defineProperty(this, 'cpfLimpo',{
-        enumerable:true,
-        get: function(){
-            return cpfEnviado.replace(/\D+/g,'');
-        }
-    });
-}
-ValidaCPF.prototype.valida = function (){
-    // o this mostra que é p cpf Limpo do objeto acima
-    if(typeof this.cpfLimpo === 'undefined') return false; 
-    if(this.cpfLimpo.length != 11)return false;    
-    if(this.isSequencia()) return false;
-    const cpfParcial = this.cpfLimpo.slice(0,-2);
-    const digito1 = this.criaDigito(cpfParcial);
-    return true;
-}
-ValidaCPF.prototype.criaDigito = function (cpfParcial){
-    const cpfArray = Array.from(cpfParcial);
-    let regressivo = cpfArray.length +1; // +1 porque precisamos de 10 e o array só tem 9 casas
-    const total = cpfArray.reduce((ac,valor) =>  {
-        console.log(regressivo,valor,regressivo* valor);
-        ac += (regressivo * Number(valor));
-        regressivo--;
-        return ac;
-    },0); // garante que começa do 0
-    const digito = 11 - (total % 11);
-    return digito > 9 ?  '0' : String(digito);
+// 705.484.450-52 070.987.720-03
+/*
+7x  0x 5x 4x 8x 4x 4x 5x 0x
+10  9  8  7  6  5  4  3  2
+70  0  40 28 48 20 16 15 0 = 237
 
-}
-ValidaCPF.prototype.isSequencia = function(){
+11 - (237 % 11) = 5 (Primeiro dígito)
+Se o número digito for maior que 9, consideramos 0.
+
+7x  0x 5x 4x 8x 4x 4x 5x 0x 5x
+11 10  9  8  7  6  5  4  3  2
+77  0  45 32 56 24 20 20 0  10 = 284
+
+11 - (284 % 11) = 2 (Primeiro dígito)
+Se o número digito for maior que 9, consideramos 0.
+*/
+function ValidaCPF(cpfEnviado) {
+    Object.defineProperty(this, 'cpfLimpo', {
+      enumerable: true,
+      get: function() {
+        return cpfEnviado.replace(/\D+/g, '');
+      }
+    });
+  }
+  
+  ValidaCPF.prototype.valida = function() {
+    if(typeof this.cpfLimpo === 'undefined') return false;
+    if(this.cpfLimpo.length !== 11) return false;
+    if(this.isSequencia()) return false;
+  
+    const cpfParcial = this.cpfLimpo.slice(0, -2);
+    const digito1 = this.criaDigito(cpfParcial);
+    const digito2 = this.criaDigito(cpfParcial + digito1);
+  
+    const novoCpf = cpfParcial + digito1 + digito2;
+    return novoCpf === this.cpfLimpo;
+  };
+  
+  ValidaCPF.prototype.criaDigito = function(cpfParcial) {
+    const cpfArray = Array.from(cpfParcial);
+  
+    let regressivo = cpfArray.length + 1;
+    const total = cpfArray.reduce((ac, val) => {
+      ac += (regressivo * Number(val));
+      regressivo--;
+      return ac;
+    }, 0); // garante que começa do 0
+  
+    const digito = 11 - (total % 11);
+    return digito > 9 ? '0' : String(digito);
+  };
+  
+  ValidaCPF.prototype.isSequencia = function() {
     const sequencia = this.cpfLimpo[0].repeat(this.cpfLimpo.length);
     return sequencia === this.cpfLimpo;
-};
-
-const cpf = new ValidaCPF('705.484.450-52');
-if(cpf.valida()){
-    console.log('cpf Valido');
-} else {
-    console.log('cpf Invalido');
-}
-console.log(cpf.valida());//cpf.valida();
+  };
+  
+  const cpf = new ValidaCPF('705.484.450-52');
+  console.log(cpf.valida());
+  if(cpf.valida()) {
+    console.log('Cpf válido');
+  } else {
+    console.log('Cpf inválido');
+  }
+  
